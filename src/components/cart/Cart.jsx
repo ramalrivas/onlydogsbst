337022a4ff62cd169642cc588396b6a82ee6a099
Ramiro Rivas <ramalrivas@gmail.com>
2022-09-22 10:59:06 -0300

    Creador un objeto usuario con orden de compra y genere un Metodo GET para crear ordenes de compra en Firebase.-

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
 import ItemCart from "../itemcart/ItemCart";
@@ -6,6 +7,23 @@ import ItemCart from "../itemcart/ItemCart";
 const Cart = () => {
   const { cart, totalPrice } = useCartContext();
 
+  const order = {
+    buyer: {
+      name:'Roberto',
+      email:'roberto@gmail.com',
+      adress:'Evergreen 1234'
+    },
+    products: cart.map(product => ({name: product.name, quantity: product.quantity, price: product.price})),
+    total: totalPrice(),
+  }
+  
+  const handleClick = () => {
+    const fireDb = getFirestore();
+    const buyOrders = collection(fireDb, 'orders');
+    addDoc(buyOrders, order)
+    .then(({id}) => console.log(id))
+  }
+  
   if (cart.length === 0) {
     return (
       <>
@@ -21,7 +39,7 @@ const Cart = () => {
   cart.map(product => <ItemCart key={product.id} product={product} />)
 }
   <p>Total: $ { totalPrice() }</p>
-  <button>Finalizar Compra</button>
+  <button onClick={handleClick}>Generar Pedido</button>
   </>
     
   )
